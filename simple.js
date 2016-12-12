e88f9991aaf16feecde5c6700b54b1a45dbc1124
Dario Cangialosi <dario.cangialosi@gmail.com>
2016-12-12 17:29:14 +0100

    more validation

--- simple.js
+++ simple.js
@@ -1,5 +1,5 @@
-outputMarker = 0;
-inputMarker = 0;
+outputMarkerCounter = 0;
+inputMarkerCounter = 0;
 if(typeof prompt==='undefined') prompt = require('readline-sync').prompt;
 
 memory = new Array(100).fill(0);
@@ -29,7 +29,51 @@ programArray =  [
         ['OUT',         11,             0,      0],     // 13) print a[3]
 ];
 
-program = programArray;
+programTest = [[0, 'ONE', 'EXIT', 'EXIT']];
+
+program = programNot;
+
+function validIndex(array, index){
+        return Number.isInteger(index) &&
+        index>=0 && index<=(array.length-1);
+}
+
+function validInstruction(instruction){
+        // check for 4 fields
+        if(instruction.length!=4) return false;
+
+        // check field with index 1
+        if(!validIndex(memory, instruction[1]) &&
+         instruction[1]!='ZERO' && instruction[1]!='ONE' && instruction[1]!='IN') return false;
+        
+        // check field with index 0
+        if(!validIndex(memory, instruction[0]) &&
+         instruction[0]!='PATHSEL' && instruction[0]!='OUT') return false;
+        
+        // check field with index 2
+        if(!validIndex(program, instruction[2]) && instruction[2]!='EXIT') return false;
+
+        // check field with index 3
+        if(!validIndex(program, instruction[3]) && instruction[3]!='EXIT') return false;
+
+        // all validity checks are successfully passed
+        return true;
+}
+
+function validProgram(program){
+        for(var i=0; i<program.length; i++)
+                if(!validInstruction(program[i])) return false;
+        return true;
+}
+
+function validMemory(memory){
+        for(var i=0; i<memory.length; i++)
+                if(memory[i]!=0 && memory[i]!=1) return false;
+        return true;
+}
+
+console.info('validMemory:    '+validMemory(memory));
+console.info('validProgram:   '+validProgram(program));
 
 while(true){
         
@@ -40,28 +84,60 @@ while(true){
         // origin
         origin = instruction[1];
 
-        if('number'===typeof origin) origin = memory[origin];
+        console.info('origin:         '+origin);
+
+        if(validIndex(memory, origin)) origin = memory[origin];
         else
         if(origin=='ZERO') origin = 0;
         else if(origin=='ONE') origin = 1;
         else if(origin=='IN') {
                 origin = parseInt(prompt());
-                console.log(origin+'\t\t in: '+inputMarker++);
+                console.log(origin+'\t\t in: '+inputMarkerCounter++);
                 if(origin!=0 && origin!=1){
-                        console.log('quit');
+                        console.error('forced exit: input is not 0 nor 1');
                         break;
                 }
+        } else {
+                console.error('forced exit: instruction not valid (origin field not valid)');
+                break; 
+        }
+
+        if(origin!=0 && origin!=1){
+                console.error('forced exit: origin is not 0 nor 1');
+                break;
         }
 
         // copy origin to destination
         destination = instruction[0];
+        
+        console.info('destination:    '+destination);
 
-        if('number'===typeof destination) memory[destination] = origin;
+        if(validIndex(memory, destination)) memory[destination] = origin;
         else
         if(destination=='PATHSEL') pathSel = origin;
-        else if(destination=='OUT') console.log(origin+'\t\t out: '+outputMarker++);
+        else if(destination=='OUT') console.log(origin+'\t\t out: '+outputMarkerCounter++);
+        else {
+                console.error('forced exit: instruction not valid (destination field not valid)');
+                break; 
+        }
 
         // path selection
-        programIndex=instruction[2+pathSel];
+
+        if(pathSel!=0 && pathSel!=1){
+                console.error('forced exit: pathSel is not 0 nor 1');
+                break;
+        }
+
+        nextProgramIndex = instruction[2+pathSel];
+        if(validIndex(program, nextProgramIndex)) programIndex=nextProgramIndex;
+        else
+        if(nextProgramIndex=='EXIT'){
+                programIndex=nextProgramIndex;
+                console.info('exit');
+                break; 
+        } else {
+                console.error('forced exit: instruction not valid (nextProgramIndex field not valid)');
+                break; 
+        }
 }
 
