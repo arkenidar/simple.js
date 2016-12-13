aad92d8031f47428df5606479a0faa7da8139ea4
Dario Cangialosi <dario.cangialosi@gmail.com>
2016-12-13 15:54:58 +0100

    format for storing programs (instructions and memory requirements)

--- simple.js
+++ simple.js
@@ -1,37 +1,52 @@
+programs = {}
+
+programs.not = {
+        instructions: [
+                ['PATHSEL',     'IN',           2,      1],     // 00) read pathSel
+                ['OUT',         'ZERO',         0,      0],     // 01) print 0
+                ['OUT',         'ONE',          0,      0],     // 02) print 1
+        ],
+        memorySize: 0,
+};
+
+programs.array =  {
+        instructions: [
+                [0,             'IN',           1,      1],     // 00) read x
+                [1,             'IN',           2,      2],     // 01) read i.0
+                [2,             'IN',           3,      3],     // 02) read i.1
+                ['PATHSEL',     1,              4,      5],     // 03) first level path separation
+                ['PATHSEL',     2,              6,      7],     // 04) second level path separation
+                ['PATHSEL',     2,              8,      9],     // 05) second level path separation
+                [3,             0,              10,     10],    // 06) a[0] = x (a[i]=x with i=0)
+                [4,             0,              10,     10],    // 07) a[1] = x (a[i]=x with i=1)
+                [5,             0,              10,     10],    // 08) a[2] = x (a[i]=x with i=2)
+                [6,             0,              10,     10],    // 09) a[3] = x (a[i]=x with i=3)
+                ['OUT',         3,              11,     11],    // 10) print a[0]
+                ['OUT',         4,              12,     12],    // 11) print a[1]
+                ['OUT',         5,              13,     13],    // 12) print a[2]
+                ['OUT',         6,              0,      0],     // 13) print a[3]
+        ],
+        memorySize: 7,
+};
+
+programs.test = {
+        instructions: [[0, 'ONE', 'EXIT', 'EXIT']],
+        memorySize: 1,
+};
+
+////////////////////////////////////////////////////
+
+programToLoad = programs.array;
+
+program = programToLoad.instructions;
+
 outputMarkerCounter = 0;
 inputMarkerCounter = 0;
 if(typeof prompt==='undefined') prompt = require('readline-sync').prompt;
 
-memory = new Array(100).fill(0);
+memory = new Array(programToLoad.memorySize).fill(0);
 programIndex = 0;
 pathSel = 0;
-
-programNot = [
-        ['PATHSEL',     'IN',           2,      1],     // 00) read pathSel
-        ['OUT',         'ZERO',         0,      0],     // 01) print 0
-        ['OUT',         'ONE',          0,      0],     // 02) print 1
-];
-
-programArray =  [
-        [5,             'IN',           1,      1],     // 00) read x
-        [6,             'IN',           2,      2],     // 01) read i.0
-        [7,             'IN',           3,      3],     // 02) read i.1
-        ['PATHSEL',     6,              4,      5],     // 03) first level path separation
-        ['PATHSEL',     7,              6,      7],     // 04) second level path separation
-        ['PATHSEL',     7,              8,      9],     // 05) second level path separation
-        [8,             5,              10,     10],    // 06) a[0] = x (a[i]=x with i=0)
-        [9,             5,              10,     10],    // 07) a[1] = x (a[i]=x with i=1)
-        [10,            5,              10,     10],    // 08) a[2] = x (a[i]=x with i=2)
-        [11,            5,              10,     10],    // 09) a[3] = x (a[i]=x with i=3)
-        ['OUT',         8,              11,     11],    // 10) print a[0]
-        ['OUT',         9,              12,     12],    // 11) print a[1]
-        ['OUT',         10,             13,     13],    // 12) print a[2]
-        ['OUT',         11,             0,      0],     // 13) print a[3]
-];
-
-programTest = [[0, 'ONE', 'EXIT', 'EXIT']];
-
-program = programNot;
 
 function validIndex(array, index){
         return Number.isInteger(index) &&
@@ -92,7 +107,7 @@ while(true){
         else if(origin=='ONE') origin = 1;
         else if(origin=='IN') {
                 origin = parseInt(prompt());
-                console.log(origin+'\t\t in: '+inputMarkerCounter++);
+                console.log(origin+'\t\t in: #'+inputMarkerCounter++);
                 if(origin!=0 && origin!=1){
                         console.error('forced exit: input is not 0 nor 1');
                         break;
@@ -115,7 +130,7 @@ while(true){
         if(validIndex(memory, destination)) memory[destination] = origin;
         else
         if(destination=='PATHSEL') pathSel = origin;
-        else if(destination=='OUT') console.log(origin+'\t\t out: '+outputMarkerCounter++);
+        else if(destination=='OUT') console.log(origin+'\t\t out: #'+outputMarkerCounter++);
         else {
                 console.error('forced exit: instruction not valid (destination field not valid)');
                 break; 
